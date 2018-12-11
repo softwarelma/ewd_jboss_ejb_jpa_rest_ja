ff395cdd9a90ab5cc926e121d06c9eb1921d911b
Guillermo Ellison <guillermo.ellison@eng.it>
2018-12-11 18:27:31 +0100

    servlet dependency

--- src/main/java/com/softwarelma/ers/main/ErsMainRS.java
+++ src/main/java/com/softwarelma/ers/main/ErsMainRS.java
@@ -1,18 +1,24 @@
 package com.softwarelma.ers.main;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 @Path("/message")
 public class ErsMainRS {
 
-	@GET
-	@Path("/{param}")
-	public Response printMessage(@PathParam("param") String msg) {
-		String result = "Restful example : " + msg;
-		return Response.status(200).entity(result).build();
-	}
+    @Context
+    private HttpServletRequest request;
+
+    @GET
+    @Path("/{param}")
+    public Response printMessage(@PathParam("param") String msg) {
+        // request.getRemoteAddr()
+        String result = "Restful example : " + msg;
+        return Response.status(200).entity(result).build();
+    }
 
 }
